cc285babf274271de3bb32ff42cf8f423f12f737
AlTeveDev <73194036+AlTeveDev@users.noreply.github.com>
2024-07-26 04:35:17 +0300

    Update windowcreator.h

--- windowcreator.h
+++ windowcreator.h
@@ -4,6 +4,7 @@
 #include <QObject>
 #include <QQmlEngine>
 #include <QQuickWindow>
+#include <QQmlComponent>
 
 class WindowCreator : public QObject
 {
@@ -28,6 +29,7 @@ private:
     void getWindow(QQmlComponent *component);
 
     QQmlEngine m_engine;
+    QQmlComponent *m_createdComponent = nullptr;
     QQuickWindow *m_createdWindow = nullptr;
 };
 
