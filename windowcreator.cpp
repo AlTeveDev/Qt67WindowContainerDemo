1aa3db492c86531828f11439c5b0bd5c70ec9d4b
AlTeveDev <73194036+AlTeveDev@users.noreply.github.com>
2024-07-26 03:50:19 +0300

    Update windowcreator.cpp

--- windowcreator.cpp
+++ windowcreator.cpp
@@ -13,25 +13,6 @@ WindowCreator::~WindowCreator() {
     qDebug() << "window creator broke";
 }
 
-void WindowCreator::getWindow(QQmlComponent *component) {
-    if (component->status() == QQmlComponent::Error) {
-        emit error(component->errorString());
-        return;
-    }
-    QObject *object = component->create();
-    m_createdWindow = qobject_cast<QQuickWindow*>(object);
-    if (!m_createdWindow) {
-        QQuickItem *item = qobject_cast<QQuickItem*>(object);
-        qDebug() << "Creato windo";
-        m_createdWindow = new QQuickWindow();
-        item->setParentItem(m_createdWindow->contentItem());
-        m_createdWindow->show();
-        emit windowCreated(m_createdWindow);
-        return;
-    }
-    emit windowCreated(m_createdWindow);
-}
-
 void WindowCreator::createWindow() {
     if (m_createdWindow) m_createdWindow->deleteLater();
     QQmlComponent *component = new QQmlComponent(&m_engine, QUrl(QStringLiteral("qrc:/WindowInWindow/Main.qml")), &m_engine);
@@ -44,4 +25,28 @@ void WindowCreator::createWindow() {
 void WindowCreator::closeWindow() {
     m_createdWindow->deleteLater();
     m_createdWindow = nullptr;
+}
+
+QQuickWindow *window() {
+    return m_createdWindow;
+}
+
+void WindowCreator::getWindow(QQmlComponent *component) {
+    if (component->status() == QQmlComponent::Error) {
+        emit error(component->errorString());
+        m_createdWindow = nullptr;
+        emit windowChanged();
+        return;
+    }
+
+    QObject *object = component->create();
+    m_createdWindow = qobject_cast<QQuickWindow*>(object);
+    if (!m_createdWindow) {
+        QQuickItem *item = qobject_cast<QQuickItem*>(object);
+        qDebug() << "Creato windo";
+        m_createdWindow = new QQuickWindow();
+        item->setParentItem(m_createdWindow->contentItem());
+        m_createdWindow->show();
+    }
+    emit windowChanged();
 }
